632dfdf27361e7e852f702a306da08fd7656edea
Pablo Ramon <ramonparaiba@gmail.com>
2023-04-06 18:48:24 -0300

    Aula Server URL

--- 001-primeiroWS.js
+++ 001-primeiroWS.js
@@ -1,6 +1,8 @@
+const { log } = require('console')
 const http = require('http')
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Primeiro web service')
-}).listen(8080)
+    res.write('Primeiro web service!!')
+    res.end()
+}).listen(8080, ()=>{console.log('Rodando na porta 8080');})
